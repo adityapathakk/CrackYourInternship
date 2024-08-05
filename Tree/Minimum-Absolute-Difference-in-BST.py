8923c2f4e570b766624fa9e98d4d5a65d1e47dba
Aditya Pathak <91721440+adityapathakk@users.noreply.github.com>
2024-08-05 19:47:20 +0530

    4 tree problems

---
+++ Tree/Minimum-Absolute-Difference-in-BST.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def getMinimumDifference(self, root: Optional[TreeNode]) -> int:
+        self.prev, self.ans = None, float('inf')
+        def inorder(root):
+            if not root: return
+            inorder(root.left)
+            if self.prev:
+                self.ans = min(self.ans, root.val - self.prev.val)
+            self.prev = root
+            inorder(root.right)
+            return
+        
+        inorder(root)
+        return self.ans
